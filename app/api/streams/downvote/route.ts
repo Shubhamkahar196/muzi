0f088b55504489c1fe3a6efa4ddb4fb1912fad4d
Shubham kahar <Shubhamkahar196@gmail.com>
2025-12-21 11:27:18 +0530

    Added upvote schema and route

---
+++ app/api/streams/downvote/route.ts
@@ -0,0 +1,47 @@
+import { prismaClient } from "@/app/lib/db";
+import UpVoteSchema from "@/app/schemas/UpVoteSchema";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const session = await getServerSession();
+
+  const user = await prismaClient.user.findFirst({
+    where: {
+      email: session?.user?.email ?? "",
+    },
+  });
+
+  if (!user) {
+    return NextResponse.json(
+      { message: "Unauthenticated" },
+      { status: 403 }
+    );
+  }
+
+  try {
+    const data = UpVoteSchema.parse(await req.json());
+
+    await prismaClient.upvote.create({
+      data: {
+        userId: user.id,
+        streamId: data.streamId,
+      },
+    });
+
+    return NextResponse.json(
+      { message: "Upvoted successfully" },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message:
+          error instanceof Error
+            ? error.message
+            : "Something went wrong",
+      },
+      { status: 411 }
+    );
+  }
+}
