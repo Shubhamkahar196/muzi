de95dada8c5790ca84393e60093a71d14eaaf42b
Shubham kahar <Shubhamkahar196@gmail.com>
2025-12-26 11:37:41 +0530

    added script

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from '../generated/prisma/client'
+// import { PrismaClient } from "@prisma/client";
+
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
